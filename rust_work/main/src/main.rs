909d5474b65fabdbcb80a85743ec92512536a681
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-03 06:49:58 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,7 +1,20 @@
-fn f<'a>() -> &'a str {
-    "abc".to_string().as_str()
+// fn type_name2<T>(_: &T) -> &str {
+//     std::any::type_name::<T>()
+// }
+
+macro_rules! type_name2 {
+    ($val:expr) => {{
+        fn type_name2<T>(_: &T) -> &str {
+            std::any::type_name::<T>()
+        }
+        type_name2(&$val)
+    }};
 }
 
 fn main() {
-    let a = f();
+    let name = "abc".to_string();
+    println!("{}", type_name2!(10 + 1));
+    println!("{}", type_name2!("abc".to_string()));
+    println!("{}", type_name2!(name)); // do not take ownership
+    let name = name; // still in use
 }

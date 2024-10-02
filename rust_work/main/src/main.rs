836abf19090a9b5605b6261ff11ab14a3cca0128
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-03 01:34:56 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,15 +1,7 @@
-fn type_name2<'a, T>(_: T) -> &'a str {
-    std::any::type_name::<T>()
+fn f<'a>() -> &'a str {
+    "abc".to_string().as_str()
 }
 
 fn main() {
-    let name = "abc";
-    let num = 123;
-    dbg!(format!("{}", type_name2(name)));
-    dbg!(format!("{}", type_name2(num + 1)));
+    let a = f();
 }
-
-/*
-[main/src/main.rs:8:5] format!("{}", type_name2(name)) = "&str"
-[main/src/main.rs:9:5] format!("{}", type_name2(num + 1)) = "i32"
-*/

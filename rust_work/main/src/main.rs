a7cf8c575b64e62f8c4e8207b47827ecbbce5519
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-03 07:37:11 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,17 +1,16 @@
-// fn type_name2<T>(_: &T) -> &str {
-//     std::any::type_name::<T>()
-// }
-
 macro_rules! type_name2 {
-    ($val:expr) => {{
+    ($arg:expr) => {{
         fn type_name2<T>(_: &T) -> &str {
             std::any::type_name::<T>()
         }
-        type_name2(&$val)
+        type_name2(&$arg)
     }};
 }
 
 fn main() {
+    let x = 5;
+    let y = Box::new(x);
+
     let name = "abc".to_string();
     println!("{}", type_name2!(10 + 1));
     println!("{}", type_name2!("abc".to_string()));

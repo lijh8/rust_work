5963ecffa09de238dbe56176606564d91f65347c
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-02 15:43:18 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -5,11 +5,18 @@ macro_rules! println2 {
     };
 }
 
-fn main2() {
+fn main() {
     // Result
 
     let result: Result<i32, &str> = Ok(123);
     // let result: Result<i32, &str> = Err("Error message");
+
+    if result.is_ok() {
+        println2!("result: {}", result.unwrap());
+    }
+    if result.is_err() {
+        println2!("is_err: {}", result.unwrap_err());
+    }
 
     if let Ok(value) = result {
         println2!("result: {}", value);
@@ -18,24 +25,10 @@ fn main2() {
         println2!("is_err: {e}");
     }
 
-    if result.is_ok() {
-        println2!("result: {}", result.unwrap());
-    }
-    if result.is_err() {
-        println2!("is_err");
-    }
-
     // Option
 
     let option: Option<i32> = Some(123);
     // let option: Option<i32> = None;
-
-    if let Some(value) = option {
-        println2!("option: {}", value);
-    }
-    if let None = option {
-        println2!("is_none");
-    }
 
     if option.is_some() {
         println2!("option: {}", option.unwrap());
@@ -43,18 +36,11 @@ fn main2() {
     if option.is_none() {
         println2!("is_none");
     }
-}
 
-fn main() {
-    let result: Result<i32, &str> = Ok(123);
-    let result: Result<i32, &str> = Err("Error message");
-    // let a = result.unwrap(); // unwrap will call panic in case of Err.
-    let a = result.unwrap_or_default();
-    println2!("{a}");
-
-    let option: Option<i32> = Some(123);
-    let option: Option<i32> = None;
-    // let b = option.unwrap(); // unwrap will call panic in case of None.
-    let b = option.unwrap_or_default();
-    println2!("{b}");
+    if let Some(value) = option {
+        println2!("option: {}", value);
+    }
+    if let None = option {
+        println2!("is_none");
+    }
 }

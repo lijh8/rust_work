ef7c22f28c23e42ad27d9c9275064266c2ff2d08
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-02 14:45:30 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,33 +1,60 @@
-/*
-mod bar; // pub required for access from outside the crate
-mod foo;
-use foo::bar as bar2; // sub-directory src/foo/bar.rs imported in foo.fs
-
-fn main2() {
-    println!("main");
-    foo::foo();
-    bar::bar();
-    bar2::bar();
+macro_rules! println2 {
+    ($($args:tt)*) => {
+        let message = format!("{}:{}: {}", file!(), line!(), format_args!($($args)*));
+        println!("{message}");
+    };
 }
 
-*/
-use log2::init_logging;
-use log2::{debug, error, info, trace, warn};
+fn main2() {
+    // Result
+
+    let result: Result<i32, &str> = Ok(123);
+    // let result: Result<i32, &str> = Err("Error message");
+
+    if let Ok(value) = result {
+        println2!("result: {}", value);
+    }
+    if let Err(e) = result {
+        println2!("is_err: {e}");
+    }
+
+    if result.is_ok() {
+        println2!("result: {}", result.unwrap());
+    }
+    if result.is_err() {
+        println2!("is_err");
+    }
+
+    // Option
+
+    let option: Option<i32> = Some(123);
+    // let option: Option<i32> = None;
+
+    if let Some(value) = option {
+        println2!("option: {}", value);
+    }
+    if let None = option {
+        println2!("is_none");
+    }
+
+    if option.is_some() {
+        println2!("option: {}", option.unwrap());
+    }
+    if option.is_none() {
+        println2!("is_none");
+    }
+}
 
 fn main() {
-    let dir = "./logs";
-    let name = "foo";
-    let _ = match init_logging(dir, name) {
-        Ok(_) => (),
-        Err(e) => println!("{e}"),
-    };
+    let result: Result<i32, &str> = Ok(123);
+    let result: Result<i32, &str> = Err("Error message");
+    // let a = result.unwrap(); // unwrap will call panic in case of Err.
+    let a = result.unwrap_or_default();
+    println2!("{a}");
 
-    // Generate some log messages to trigger rolling
-    loop {
-        error!("This is error message!");
-        warn!("This is warn message!");
-        info!("This is info message!");
-        debug!("This is debug message!");
-        trace!("This is trace message!");
-    }
+    let option: Option<i32> = Some(123);
+    let option: Option<i32> = None;
+    // let b = option.unwrap(); // unwrap will call panic in case of None.
+    let b = option.unwrap_or_default();
+    println2!("{b}");
 }

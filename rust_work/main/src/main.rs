7d40b327df40aa5915c4fc8f57bbe126ba8f6de9
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-04 16:28:05 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,28 +1,49 @@
-macro_rules! type_name2 {
-    ($arg:expr) => {{
-        fn type_name2<T: ?Sized>(_: &T) -> &str {
-            std::any::type_name::<T>()
-        }
-        type_name2(&$arg)
-    }};
+// macro_rules! type_name2 {
+//     ($arg:expr) => {{
+//         fn type_name2<T: ?Sized>(_: &T) -> &str {
+//             std::any::type_name::<T>()
+//         }
+//         type_name2(&$arg)
+//     }};
+// }
+
+//
+
+// let name = "abc"
+// let num = 123
+
+// dbg!(format!("{}, {}", name, num));
+// dbg2!("{}, {}", name, num);
+macro_rules! dbg2 {
+    ($($args:tt)*) => {
+        dbg!(format_args!($($args)*))
+    };
+}
+
+// println2!("{}, {}", name, num);
+macro_rules! println2 {
+    ($($args:tt)*) => {
+        println!("{}:{}: {}", file!(), line!(), format_args!($($args)*));
+    };
 }
 
 //
 
 fn main() {
-    let v = vec![1, 2, 3];
-    let a = [1, 2, 3];
+    let text = "123".to_string();
+    let mut num: i32 = 0;
+    // if let Ok(x) = text.parse::<i32>() {
+    //     num = x;
+    //     // dbg!(format!("{}, {}", text, num));
+    // };
+    if text.parse::<i32>().is_ok() {
+        num = text.parse::<i32>().unwrap();
+    }
 
-    dbg!(format!("{}", type_name2!(v)));
-    dbg!(format!("{}", type_name2!(a)));
-    dbg!(format!("{}", type_name2!(&v)));
-    dbg!(format!("{}", type_name2!(&a)));
-    dbg!(format!("{}", type_name2!(&v[..])));
-    dbg!(format!("{}", type_name2!(&a[..])));
-
-    dbg!(format!("{}", type_name2!(v[..])));
-    dbg!(format!("{}", type_name2!(a[..])));
-
-    let a2 = a;
-    let v2 = v;
+    dbg!(format!("{}, {}", text, num));
+    dbg!(format!("{}, {}", text, num));
+    dbg2!("{:?}, {:?}", text, num);
+    dbg2!("{:?}, {:?}", text, num);
+    println2!("{:?}, {:?}", text, num);
+    println2!("{:?}, {:?}", text, num);
 }

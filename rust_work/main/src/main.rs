42b2aefd0c4b6399ab259c18baf610d0a881eefe
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-04 21:20:12 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,49 +1,23 @@
-// macro_rules! type_name2 {
-//     ($arg:expr) => {{
-//         fn type_name2<T: ?Sized>(_: &T) -> &str {
-//             std::any::type_name::<T>()
-//         }
-//         type_name2(&$arg)
-//     }};
-// }
-
-//
-
-// let name = "abc"
-// let num = 123
-
-// dbg!(format!("{}, {}", name, num));
-// dbg2!("{}, {}", name, num);
-macro_rules! dbg2 {
-    ($($args:tt)*) => {
-        dbg!(format_args!($($args)*))
-    };
-}
-
-// println2!("{}, {}", name, num);
-macro_rules! println2 {
-    ($($args:tt)*) => {
-        println!("{}:{}: {}", file!(), line!(), format_args!($($args)*));
-    };
+macro_rules! type_name2 {
+    ($arg:expr) => {{
+        fn type_name2<T: ?Sized>(_: &T) -> &str {
+            std::any::type_name::<T>()
+        }
+        type_name2(&$arg)
+    }};
 }
 
 //
 
 fn main() {
-    let text = "123".to_string();
-    let mut num: i32 = 0;
-    // if let Ok(x) = text.parse::<i32>() {
-    //     num = x;
-    //     // dbg!(format!("{}, {}", text, num));
-    // };
-    if text.parse::<i32>().is_ok() {
-        num = text.parse::<i32>().unwrap();
-    }
+    let haystack = [1, 2, 3, 3, 3, 4, 5];
+    let needle = 3;
 
-    dbg!(format!("{}, {}", text, num));
-    dbg!(format!("{}, {}", text, num));
-    dbg2!("{:?}, {:?}", text, num);
-    dbg2!("{:?}, {:?}", text, num);
-    println2!("{:?}, {:?}", text, num);
-    println2!("{:?}, {:?}", text, num);
+    let begin = haystack.partition_point(|x| x < &needle);
+    let end = haystack.partition_point(|x| x <= &needle);
+    if begin == end {
+        dbg!(format_args!("not found, {}, {}", begin, end));
+    } else {
+        dbg!(format_args!("found, {}, {}", begin, end));
+    }
 }

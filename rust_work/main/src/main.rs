8997d77a67b21e1c8d7e69cd3ccd096b5e2637de
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-05 05:51:43 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,23 +1,9 @@
-macro_rules! type_name2 {
-    ($arg:expr) => {{
-        fn type_name2<T: ?Sized>(_: &T) -> &str {
-            std::any::type_name::<T>()
-        }
-        type_name2(&$arg)
-    }};
-}
-
-//
+use main::add;
+use main::add2::add2;
 
 fn main() {
-    let haystack = [1, 2, 3, 3, 3, 4, 5];
-    let needle = 3;
-
-    let begin = haystack.partition_point(|x| x < &needle);
-    let end = haystack.partition_point(|x| x <= &needle);
-    if begin == end {
-        dbg!(format_args!("not found, {}, {}", begin, end));
-    } else {
-        dbg!(format_args!("found, {}, {}", begin, end));
-    }
+    let sum = add(1, 2);
+    dbg!(format_args!("{}", sum));
+    let sum = add2(1, 2);
+    dbg!(format_args!("{}", sum));
 }

a2878cb7b0d4dfa70f9e2fd2dcb13937b790a41c
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-05 15:53:54 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,9 +1,9 @@
-use main::add;
-use main::add2::add2;
+// use main::add;
+// use main::add2::add2;
 
 fn main() {
-    let sum = add(1, 2);
-    dbg!(format_args!("{}", sum));
-    let sum = add2(1, 2);
-    dbg!(format_args!("{}", sum));
+    // let sum = add(1, 2);
+    // dbg!(format_args!("{}", sum));
+    // let sum = add2(1, 2);
+    // dbg!(format_args!("{}", sum));
 }

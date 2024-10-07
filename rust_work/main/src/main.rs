fc581eb4388001a4c3236fdbb5008d65a906fad8
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-08 04:07:49 +0800

    Add files via upload

--- rust_work/main/src/main.rs
+++ rust_work/main/src/main.rs
@@ -1,31 +1,29 @@
-macro_rules! type_name2 {
-    ($arg:expr) => {{
-        fn type_name2<T: ?Sized>(_: &T) -> &str {
-            std::any::type_name::<T>()
-        }
-        type_name2(&$arg)
-    }};
-}
+// macro_rules! type_name2 {
+//     ($arg:expr) => {{
+//         fn type_name2<T: ?Sized>(_: &T) -> &str {
+//             std::any::type_name::<T>()
+//         }
+//         type_name2(&$arg)
+//     }};
+// }
 
 //
 
-use log2::init_logging;
-use log2::{debug, error, info, trace, warn};
+fn foo<'a, 'b>(a: &'a String, b: &'b String) -> &'a String {
+    // if a.len() > b.len() {
+    //     a
+    // } else {
+    //     b
+    // }
+    a
+}
 
 fn main() {
-    let dir = "./logs";
-    let name = "foo";
-    let _ = match init_logging(dir, name) {
-        Ok(_) => (),
-        Err(e) => println!("{e}"),
-    };
-
-    // Generate some log messages to trigger rolling
-    loop {
-        error!("This is error message!");
-        warn!("This is warn message!");
-        info!("This is info message!");
-        debug!("This is debug message!");
-        trace!("This is trace message!");
+    let a = String::from("abc");
+    let b = String::from("abcdef");
+    let c;
+    {
+        c = foo(&b, &a);
     }
+    dbg!(&c);
 }
